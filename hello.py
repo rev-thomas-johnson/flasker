8992f0ccea7b880b60e7afca6fc666e78e970a4e
Tom Johnson <thomas.johnson@revature.net>
2022-04-14 15:06:43 -0500

    lesson 5

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create a Flask instance
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+	name = StringField("What's Your Name?", validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
 # Create a route decorator
 
@@ -33,6 +41,23 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template("500.html"), 500
+
+
+# Create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+	# Validate Form
+	if form.validate_on_submit():
+		name = form.name.data 
+		form.name.data = ''
+
+	return render_template("name.html",
+		name = name,
+		form = form)
+
+
 
 
 # Filters
