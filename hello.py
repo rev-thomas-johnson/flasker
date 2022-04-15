81acfef9c49579f501aad4e7d386a8ae0b42528b
Tom Johnson <thomas.johnson@revature.net>
2022-04-15 15:11:15 -0500

    lesson 10

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -45,6 +45,30 @@ class NamerForm(FlaskForm):
 	submit = SubmitField("Submit")
 
 # Create a route decorator
+
+# Update Database Record
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+	form = UserForm()
+	name_to_update = Users.query.get_or_404(id)
+	if request.method == "POST":
+		name_to_update.name = request.form['name']
+		name_to_update.email = request.form['email']
+		try:
+			db.session.commit()
+			flash('User Updated Successfully')
+			return render_template("update.html",
+				form=form,
+				name_to_update=name_to_update)
+		except:
+			flash('Error...Update Unsuccessful.')
+			return render_template("update.html",
+				form=form,
+				name_to_update=name_to_update)
+	else:
+		return render_template("update.html",
+			form=form,
+			name_to_update=name_to_update)
 
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
