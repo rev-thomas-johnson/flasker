f5837f3384107660c4e6e41c08a06b2dedde2265
Tom Johnson <thomas.johnson@revature.net>
2022-04-15 09:09:36 -0500

    lesson 8

--- hello.py
+++ hello.py
@@ -2,11 +2,36 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 # Create a Flask instance
 app = Flask(__name__)
+# Add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# Secret Key
 app.config['SECRET_KEY'] = "my secret key"
+# Initialize the Database
+db = SQLAlchemy(app)
+
+# Create Model
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	email = db.Column(db.String(120), nullable=False, unique=True)
+	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+	# Create String
+	def __repr__(self):
+		return '<Name %r>' % self.name
+
+# Create a Form Class
+class UserForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	email = StringField("Email", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
 
 # Create a Form Class
 class NamerForm(FlaskForm):
@@ -14,6 +39,26 @@ class NamerForm(FlaskForm):
 	submit = SubmitField("Submit")
 
 # Create a route decorator
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+	name = None
+	form = UserForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user is None:
+			user = Users(name=form.name.data, email=form.email.data)
+			db.session.add(user)
+			db.session.commit()
+		name = form.name.data
+		form.name.data = ''
+		form.email.data = ''
+		flash("User added successfully!")
+	our_users = Users.query.order_by(Users.date_added)
+	return render_template("add_user.html", 
+		form=form,
+		name=name,
+		our_users=our_users)
 
 @app.route('/')
 def index():
